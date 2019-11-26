ed35bee6b5badea36b5c8c713b5239760ee7c25a
hongha1182000 <ha11882000@gmail.com>
2019-11-26 09:13:32 +0700

    This is a new commit for what I originally planned to be amended

---
+++ hello.js
@@ -0,0 +1,34 @@
+console.log('Hello World');
+console.log('cuong ko khon ');
+let name= 'Ha sua on air';
+let onAIR=' ha sua yeu hien'
+console.log("i'm "+ name)
+console.log('moi nguoi oi'+onAIR)
+console.log(`i'm ${onAIR} years old`)
+let yob = prompt(' ghi tuoi cua may vao: ');
+console.log( 2019 addEventListener+ Number(yob));
+// clear()
+// for (let i=0;i<=100;i++){
+// lt(90)
+// fd(100)
+// lt(90)
+// fd(100)
+// lt(90)
+// fd(100)
+// lt(90)
+// fd(100)
+// rt(20)
+// }
+
+// clear()
+// color("blue")
+// for(i=0; i<180; i++){
+//   lt(1)
+//   for(j=0; j<360; j++){
+//     lt(1)
+//     fd(1)
+//   }
+// }
+
+ 
+
